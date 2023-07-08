ca9112a9484c67d41575cec6a69e977af9442c93
Zach Bogart <>
2023-07-08 11:31:12 -0400

    reformat as individual folder, apple trademarks

---
+++ apple-trademarks/wrangle-trademarks.R
@@ -0,0 +1,12 @@
+library(tidyverse)
+library(here)
+library(readxl)
+
+raw <- read_excel(here("apple-trademarks/data/apple-legal-trademark-list.xlsx"))
+
+apple_trademarks <- raw %>% 
+    select(name) %>% 
+    mutate(name_no_mark = str_remove(name, "[®™℠ ]$"))
+
+apple_trademarks %>% 
+    write_csv(here("apple-trademarks/output/apple-trademarks.csv"))
